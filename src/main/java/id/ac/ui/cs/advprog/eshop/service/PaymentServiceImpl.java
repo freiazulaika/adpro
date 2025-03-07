e35b269324a86c1594768d9b79d669e71c29cf2c
freiazulaika <freiazulaika27@gmail.com>
2025-03-07 09:58:25 +0700

    [GREEN] Implement PaymentServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
+import enums.PaymentMethod;
+import enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -17,21 +20,43 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
+        if (order == null) {
+            throw new IllegalArgumentException("Order cannot be null");
+        }
+
+        if (!PaymentMethod.contains(method)) {
+            throw new IllegalArgumentException("Unsupported payment method: " + method);
+        }
+
+        String paymentId = "payment-" + UUID.randomUUID().toString().substring(0, 8);
+
+        Payment payment = new Payment(paymentId, method, paymentData);
+
+        return paymentRepository.save(order, payment);
     }
 
     @Override
     public Payment setStatus(Payment payment, String status) {
-        return null;
+        if (payment == null) {
+            throw new IllegalArgumentException("Payment cannot be null");
+        }
+
+        if (!PaymentStatus.contains(status)) {
+            throw new IllegalArgumentException("Invalid payment status: " + status);
+        }
+
+        paymentRepository.update(payment, status);
+
+        return payment;
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.findAll();
     }
 }
